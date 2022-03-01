fb1f5815f5a6c3c7eccb9b49e863687f89cdbcaa
Nebularswamp <grosswamp@gmail.com>
2022-02-28 19:55:03 -0500

    Added name / team color customization

--- Assets/_scripts/CustomizeCharacter.cs
+++ Assets/_scripts/CustomizeCharacter.cs
@@ -1,25 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
+// rewrite this 
 
 public class CustomizeCharacter : MonoBehaviour
 {
+    public InputField myName; //stores the player's name
+    public GameObject playerName; //refers to the text mesh display above player
     public GameObject[] hair;
     private int currentHair;
     public GameObject[] coat;
     private int currentCoat;
 
+    public Color[] teamColor;
     public Color[] skinColor;
     public Color[] hairColor;
     public Color[] coatColor;
     public Color[] pantsColor;
     public Color[] bootsColor;
 
+    public Material teamMat;
     public Material skinMat;
     public Material hairMat;
     public Material coatMat;
     public Material pantsMat;
     public Material bootsMat;
+
+    
 
     public void ChangeHair()
     {
@@ -37,6 +47,18 @@ public class CustomizeCharacter : MonoBehaviour
             hair[i].SetActive(false);
         }
         hair[currentHair].SetActive(true);
+    }
+
+    public void ChangeName(string name)
+    {
+        name = myName.text;
+        playerName.transform.GetChild(0).GetComponent<TextMesh>().text = name;
+    }
+
+    public void ChangeTeamColor(int index)
+    {
+        teamMat.color = teamColor[index];
+        playerName.transform.GetChild(0).GetComponent<TextMesh>().color = teamColor[index];
     }
 
     public void ChangeHairColor(int index)
@@ -63,6 +85,7 @@ public class CustomizeCharacter : MonoBehaviour
         bootsMat.color = bootsColor[index];
     }
 
+
     public void ChangeCoat()
     {
         if (currentCoat >= coat.Length - 1)
@@ -85,5 +108,10 @@ public class CustomizeCharacter : MonoBehaviour
     {
         ChangeHair();
         ChangeCoat();   
+    }
+
+    private void Update()
+    {
+        playerName.transform.LookAt(Camera.main.transform); //move this to player movement later
     }
 }
